86462ddc2b9ca4ad5c6693117ea4e6d0284fab36
Andrés Ireta <andres.ireta@unosqueare.com>
2025-01-12 02:06:58 -0600

    callbacks example

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,7 +1,18 @@
 let { emailTemplate } = require('./js-foundation/01-template');
 require('./js-foundation/02-destructuring');
+const { getUserById } = require('./js-foundation/03-callbacks');
 
-/* emailTemplate = emailTemplate.replace('{{ name }}', 'John Doe');
+/*
+emailTemplate = emailTemplate.replace('{{ name }}', 'John Doe');
 emailTemplate = emailTemplate.replace('{{ orderDetail }}', 'Order detail Amazon Echo');
-console.log(emailTemplate); */
+console.log(emailTemplate);
+*/
+
+getUserById(3, function (error, user) {
+    if (error) {
+        throw new Error(error);
+    }
+
+    console.log({user});
+});
 
--- 02-bases/src/js-foundation/02-destructuring.js
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -1,1 +1,11 @@
-console.log(process.env); 
+// console.log(process.env);
+
+const { SHELL, HOMEBREW_PREFIX, npm_lifecycle_script } = process.env;
+//console.table({ SHELL, HOMEBREW_PREFIX, npm_lifecycle_script });
+
+const characters = ['Flash', 'Arrow', 'Superman', 'Batman'];
+const [_, __, superman] = characters;
+const [flash, ...rest] = characters;
+
+//console.log(superman);
+//console.log(rest);
