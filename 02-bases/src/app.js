9b5bf6939fc9ea5fcc403e7375adcecd87cf48a8
Andrés Ireta <andres.ireta@unosqueare.com>
2025-01-12 01:30:40 -0600

    update progress and gitignore

---
+++ 02-bases/src/app.js
@@ -0,0 +1,7 @@
+let { emailTemplate } = require('./js-foundation/01-template');
+require('./js-foundation/02-destructuring');
+
+/* emailTemplate = emailTemplate.replace('{{ name }}', 'John Doe');
+emailTemplate = emailTemplate.replace('{{ orderDetail }}', 'Order detail Amazon Echo');
+console.log(emailTemplate); */
+
---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,12 @@
+
+
+const emailTemplate = `
+<div>
+    h1>Hi {{ name }}</h1>
+    <p>Thanks you for your order</p>
+    <p>Order details: {{ orderDetail }}</p>
+</div>`;
+
+module.exports = {
+    emailTemplate
+};
